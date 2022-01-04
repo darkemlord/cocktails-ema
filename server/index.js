34f0a700d186e1179e6e9f6126490d4db3cb035f
emanuel caro <darkemlord@gmail.com>
2022-01-04 22:23:12 +0900

    utilities installed and working

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
 const express = require('express');
+require('dotenv').config({ path: ".env" });
+
+console.log(process.env.MONGO_DB);
