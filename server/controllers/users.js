4b11bf5a3ea9b470786890da9dcba87ad40f6d36
emanuel caro <darkemlord@gmail.com>
2022-02-24 15:41:03 +0900

    register function updated lookingfor users and emails

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,6 +4,13 @@ const register = async (input) => {
   const newUser = input;
   newUser.email = newUser.email.toLowerCase();
   newUser.username = newUser.username.toLowerCase();
+  const { username, password, email } = newUser;
+
+  const foundEmail = await User.findOne({ email });
+  if(foundEmail) throw new Error('email already in use');
+
+  const foundUsername = await User.findOne({ username });
+  if(foundUsername) throw new Error('username already exists');
 
 }
 
