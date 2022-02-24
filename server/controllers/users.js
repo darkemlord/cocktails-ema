bd3ab2b52fb8c0cebc5be69ea792d8ca6660bcd6
emanuel caro <darkemlord@gmail.com>
2022-02-24 15:42:45 +0900

    bycrypt js installed

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,11 +6,14 @@ const register = async (input) => {
   newUser.username = newUser.username.toLowerCase();
   const { username, password, email } = newUser;
 
+  //looking for email and users
   const foundEmail = await User.findOne({ email });
   if(foundEmail) throw new Error('email already in use');
 
   const foundUsername = await User.findOne({ username });
   if(foundUsername) throw new Error('username already exists');
+
+  //encrypt password
 
 }
 
